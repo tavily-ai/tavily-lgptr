fd9b390329e41556b0b25a2d4a96e18c3fd3204e
maitarasher <maitarasher@gmail.com>
2024-11-01 12:20:12 -0400

    add config flie

---
+++ agents/config/config.py
@@ -0,0 +1,9 @@
+# Description: Configuration file
+class Config:
+    def __init__(self):
+        """
+        Initializes the configuration for the agents
+        """
+        self.MAX_SEARCH_QUERIES = 4  # Max search queries to run (including initial search query)
+        self.MAX_CURATED_DOCS = 10
+
